1a2c3eb8a26ee7e87ace9905012e385d8f6919d4
TheBizzle <jason.bertsche@gmail.com>
2014-12-16 00:35:21 -0600

    Removing code that only calls IFramePhone stuff when in CODAP
    This had both become unnecessary and caused security problems

--- codap.js
+++ codap.js
@@ -1,29 +1,23 @@
 function doCommand(arg, callback) {
-  if(window.parent && window.parent.DG) {
-    return window.codapPhone.call(arg, callback);
-  } else {
-    alert("Not in datagames, couldn't do '" + arg.action + "'!");
-  }
+  window.codapPhone.call(arg, callback);
 }
 
 function record(series) {
-  if(window.parent && window.parent.DG) {
+  doCommand({
+    action: 'openCase',
+    args: {
+      collection: "Challenge",
+      values: series
+    }
+  }, function(result) {
     doCommand({
-      action: 'openCase',
+      action: 'closeCase',
       args: {
         collection: "Challenge",
-        values: series
+        caseID: result.caseID
       }
-    }, function(result) {
-      doCommand({
-        action: 'closeCase',
-        args: {
-          collection: "Challenge",
-          caseID: result.caseID
-        }
-      });
     });
-  }
+  });
 }
 
 function logCODAPAction(message, args) {
@@ -37,35 +31,31 @@ function openCODAPTable() {
 function clearCODAPData() {
 }
 
-if(window.parent && window.parent.DG) {
+var initFunc = function(iCmd, callback) {
+  var operation = iCmd && iCmd.operation;
+  var args      = iCmd && iCmd.args;
+  switch(operation) {
+    default: callback({ success: false });
+  }
+};
 
-  var initFunc = function(iCmd, callback) {
-    var operation = iCmd && iCmd.operation;
-    var args      = iCmd && iCmd.args;
-    switch(operation) {
-      default: callback({ success: false });
-    }
-  };
+window.codapPhone = new iframePhone.IframePhoneRpcEndpoint(initFunc, "codap-game", window.parent);
 
-  window.codapPhone = new iframePhone.IframePhoneRpcEndpoint(initFunc, "codap-game", window.parent);
-
-  doCommand({
-    action: 'initGame',
-    args: {
-      name: "Ramp Game",
-      dimensions: { width: 775, height: 450 },
-      collections: [
-        {
-          name: "Challenge",
-          attrs: [ { name: "Challenge", type: "numeric", description: "BB", precision: 1 },
-                   { name: "Step", type: "numeric", description: "BB", precision: 1 },
-                   { name: "Start Height", type: "numeric", description: "AA", precision: 2, },
-                   { name: "Friction", type: "numeric", description: "BB", precision: 2 },
-                   { name: "Mass", type: "numeric", description: "BB", precision: 2 },
-                   { name: "End Distance", type: "numeric", description: "BB", precision: 2 } ],
-        }
-      ]
-    }
-  });
-
-}
+doCommand({
+  action: 'initGame',
+  args: {
+    name: "Ramp Game",
+    dimensions: { width: 775, height: 450 },
+    collections: [
+      {
+        name: "Challenge",
+        attrs: [ { name: "Challenge", type: "numeric", description: "BB", precision: 1 },
+                 { name: "Step", type: "numeric", description: "BB", precision: 1 },
+                 { name: "Start Height", type: "numeric", description: "AA", precision: 2, },
+                 { name: "Friction", type: "numeric", description: "BB", precision: 2 },
+                 { name: "Mass", type: "numeric", description: "BB", precision: 2 },
+                 { name: "End Distance", type: "numeric", description: "BB", precision: 2 } ],
+      }
+    ]
+  }
+});
